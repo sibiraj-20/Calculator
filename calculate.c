fd2b921f69843df7e5683440b81b602faca70392
sibiraj-20 <sibirajm.20cse@kongu.edu>
2021-10-29 19:09:48 +0530

    first commit

---
+++ calculate.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main ()
+{
+	int a;
+	int b;
+	int c;
+	printf("Enter the value for a :");
+	scanf("%d ",&a);
+	printf("Enter the value for b :");
+	scanf("%d ",&b);
+	c = a+b ;
+	printf("the sum is : %d",c);
+}
